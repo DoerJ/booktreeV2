1e88d5b8b96e00ab713234c64cde9210a7107dfb
jamesHe <jamesyuhao@hotmail.com>
2020-04-04 00:26:55 -0700

    Added promise-factory

--- client/src/services/apis/userapis.js
+++ client/src/services/apis/userapis.js
@@ -9,5 +9,11 @@ export const userAPIs = {
     },
     logout: (params, success, error) => {
         apiConnector('POST', '/api/user/logout', params, success, error);
+    },
+    get_meinfo: (params, success, error) => {
+        apiConnector('GET', '/api/user/get-meinfo', params, success, error);
+    }
+    get_mestats: (params, success, error) => {
+        apiConnector('GET', '/api/user/get-mestats', params, success, error);
     }
 }
